7003ad057069546100754d1ae65e2821f31f4817
benPlosk <benplosk2@gmail.com>
2021-10-15 23:24:33 +0300

    added the var method as well as tow helper methods to simplify the calculation

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -1,10 +1,43 @@
 #include "anomaly_detection_util.h"
 #include <iostream>
+#include <cmath>
 
 //this is the source file for anomaly_detection_util library
 //it's the ex1 for 89210 project
 // returns the variance of X and Y
-float var(float *x, int size);
+//helper method to calculate the sum of variables
+float sumMembers(float* x, int size) {
+    float sum = 0;
+    //loop through floats and sum them
+    for (int i = 0; i < size; i++) {
+        sum += x[i];
+    }
+    return sum;
+}
+
+//helper method to calculate the sum of squares
+float sumSquares(float* x, int size) {
+    float sum = 0;
+    //loop through floats and sum the squares of them
+    for (int i = 0; i < size; i++) {
+        sum += pow(x[i], 2);
+    }
+    return sum;
+}
+
+// returns the variance of X and Y
+float var(float* x, int size) {
+    /*add check if any of floats in x are negative?
+    add check for valid size: not 0 or negative etc */
+
+    //first some calculation preparations to simplify the formula
+    float sum = sumMembers(x, size);
+    float squareSum = sumSquares(x, size);
+    float ro = sum / size ;
+    //finally calculate variance using the formula
+    float var = (squareSum / size)  - pow(ro, 2);
+    return var;
+}
 
 // returns the covariance of X and Y
 float cov(float *x, float *y, int size);
