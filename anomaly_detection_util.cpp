0ef337cb43c27a03562562b594448f462a8faddf
benPlosk <benplosk2@gmail.com>
2021-10-16 18:14:25 +0300

    added covariance method as well as fixed avg method to use the sum method to avoid code reuse

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -25,6 +25,11 @@ float sumSquares(float* x, int size) {
     return sum;
 }
 
+float avg(float *x, int size) {
+   float sum = sumMembers(x,size);
+    return sum / (float) size;
+}
+
 // returns the variance of X and Y
 float var(float* x, int size) {
     /*add check if any of floats in x are negative?
@@ -33,14 +38,30 @@ float var(float* x, int size) {
     //first some calculation preparations to simplify the formula
     float sum = sumMembers(x, size);
     float squareSum = sumSquares(x, size);
-    float ro = sum / (float)size ;
+    float ro = avg(x,size) ;
     //finally calculate variance using the formula
     float var = (squareSum / (float)size)  - pow(ro, 2);
     return var;
 }
 
 // returns the covariance of X and Y
-float cov(float *x, float *y, int size);
+float cov(float *x, float *y, int size) {
+   //calculate avg to use later in the loop
+    float ruX = avg(x,size);
+    float ruY = avg(y,size);
+    //init sum
+    float sum = 0;
+    //sum the product of (Xi - ruX) * (Yi - ruY) like the formula said
+    for(int i =0 ;i<size; i++) {
+        float deltaX = x[i] - ruX;
+        float deltaY = y[i] - ruY;
+        sum += (deltaX*deltaY);
+    }
+    //cov is the sum divided by the population size
+    float cov = (sum / size);
+    //return the covariance
+    return cov;
+}
 
 
 // returns the Pearson correlation coefficient of X and Y
@@ -48,19 +69,8 @@ float pearson(float *x, float *y, int size);
 
 
 
-/**
- * Calculates the average of an array.
- *
- * @param filename GIF File name to be loaded
- * @return Inited GIF Handler or NULL for error
- */
-float avg(float *x, int size) {
-    float sum = 0;
-    for (int i = 0; i < size; i++) {
-        sum = sum + x[i];
-    }
-    return sum / (float) size;
-}
+
+
 
 // performs a linear regression and return s the line equation
 Line linear_reg(Point **points, int size) ;
@@ -72,11 +82,11 @@ float dev(Point p, Point **points, int size) ;
 float dev(Point p, Line l) ;
 
 int main() {
-
-    float *x;
-    float t[] = {1, 2, 3, 4, 5};
-    x = t;
-    std::cout << avg(x, 5);
+    float X[] = {2,4,6,8,10};
+    float Y[] = {12,11,8,3,1};
+    std :: cout  << "var X is" << var(X,5) << "\n"; //8
+    std :: cout << "var Y is " << var(Y,5) << "\n"; //18.8
+    std :: cout << "cov X Y is " << cov(X,Y,5) << "\n"; //-12
 
 
 }//end of the library
