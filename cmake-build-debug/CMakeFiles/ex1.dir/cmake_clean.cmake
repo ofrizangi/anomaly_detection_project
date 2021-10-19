51f94dedaa7a03959303723dc2354abb520aa44b
benPlosk <benplosk2@gmail.com>
2021-10-19 09:25:40 +0300

    added the pearson method

---
+++ cmake-build-debug/CMakeFiles/ex1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex1.dir/anomaly_detection_util.cpp.obj"
+  "ex1.exe"
+  "ex1.exe.manifest"
+  "ex1.lib"
+  "ex1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
