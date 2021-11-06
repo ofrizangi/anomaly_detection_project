275ef540cf9398777861a57ec699a9bda398d1a8
Ofri Zangi <ofrizangi@gmail.com>
2021-11-06 10:32:46 +0200

    new classes

--- TimeSeries.cpp
+++ TimeSeries.cpp
@@ -1,4 +1,57 @@
+#include <map>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+//#include "TimeSeries.h"
+
+using namespace std;
 
 
+class TimeSeries {
 
-class Ti
+public:
+    map<string, vector<float>> table;
+
+
+public:
+    TimeSeries(string fileName) {
+        ifstream readFile(fileName);
+        string names;
+        getline(readFile, names);
+        vector<string> properties = splitBy(names, ',');
+        vector<float> arr[properties.size()];
+
+        vector<string>::iterator it;
+        vector<string> values;
+        while (getline(readFile, names)) {
+            values = splitBy(names, ',');
+            int place = 0;
+            for (it = values.begin(); it != values.end(); it++) {
+                arr[place].push_back(stof(*it));
+                place++;
+                it++;
+            }
+        }
+        vector<string>::iterator itr;
+        for (itr = properties.begin(); itr != properties.end(); it++) {
+            this->table.insert({*itr, arr[0]});
+        }
+    }
+
+
+    vector<string> splitBy(string s, char del) {
+        int start = 0;
+        int end = s.find(del);
+        vector<string> names;
+        while (end != -1) {
+            names.push_back(s.substr(start, end - start));
+            start = end + 1;
+            end = s.find(del, start);
+        }
+        names.push_back(s.substr(start, s.length() - 1));
+        return names;
+    }
+
+
+};
--- TimeSeries.h
+++ TimeSeries.h
@@ -1,14 +1,21 @@
-//
-// Created by Ofri on 11/5/2021.
-//
+#include <vector>
+#include <map>
+using namespace std;
+
 
 #ifndef TIMESERIESPROJECT_TIMESERIES_H
 #define TIMESERIESPROJECT_TIMESERIES_H
 
 
 class TimeSeries {
-
+public:
+    map<string, vector<float>> table;
+public:
+    TimeSeries(string s);
 };
 
+//std::vector <std::string> splitBy(std::string s, char tav);
 
-#endif //TIMESERIESPROJECT_TIMESERIES_H
+
+
+#endif
